7acf38acd59e865982193884a116403a8902e328
mexell96 <mexell96@gmail.com>
2021-01-25 22:33:13 +0300

    added Radium

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import Radium from "radium";
 import "./Car.css";
 
-export default (props) => {
+const Car = (props) => {
   const inputClasses = ["input"];
 
   if (props.name !== "") {
@@ -14,8 +15,18 @@ export default (props) => {
     inputClasses.push("bold");
   }
 
+  const style = {
+    border: "1px solid black",
+    boxShadow: "0 4px 5px 0 rgba(0,0,0, 0.14)",
+    ":hover": {
+      border: "1px solid #aaa",
+      boxShadow: "0 4px 15px 0 rgba(0,0,0 .25)",
+      cursor: "pointer",
+    },
+  };
+
   return (
-    <div className="Car">
+    <div className="Car" style={style}>
       <h3>Car name: {props.name}</h3>
       <p>
         Year: <strong>{props.year}</strong>
@@ -30,3 +41,5 @@ export default (props) => {
     </div>
   );
 };
+
+export default Radium(Car);
