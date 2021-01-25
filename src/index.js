3cc4ea8ab92b8d6d1f201d10a71057a96b8fe13c
mexell96 <mexell96@gmail.com>
2021-01-25 11:34:22 +0300

    lesson-3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Cars = (
+  <>
+    <App name="Mazda" age="10" />,
+    <App name="Opel" age="6" />,
+    <App name="Ford" age="7" />,
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(Cars, document.getElementById("root"));
